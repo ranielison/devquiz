470b5ed47ef165bf6253e52b50677d3b3e72f253
Ranielison <ranielisonsoares2@gmail.com>
2021-04-21 00:23:00 -0300

    feat: criando componentes da home peage

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,37 +9,49 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(
-              gradient: AppGradients.linear,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Stack(
               children: [
-                Text.rich(
-                  TextSpan(
-                    text: "Olá, ",
-                    style: AppTextStyles.title,
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      TextSpan(
-                        text: "Raniélison",
-                        style: AppTextStyles.titleBold,
+                      Text.rich(
+                        TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: "Raniélison",
+                              style: AppTextStyles.titleBold,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 58,
+                        width: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/16888639?s=400&u=a757561747f36002493dc25f2c937c544c40a3d0&v=4",
+                            ),
+                          ),
+                        ),
                       )
                     ],
                   ),
                 ),
-                Container(
-                  height: 58,
-                  width: 58,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/16888639?s=400&u=a757561747f36002493dc25f2c937c544c40a3d0&v=4",
-                      ),
-                    ),
-                  ),
-                )
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
               ],
             ),
           ),
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,39 @@
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 136,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: AppColors.white,
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ChartWidget(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Vamos começar', style: AppTextStyles.heading),
+                    Text(
+                      'Complete os desafios e avance em conhecimento',
+                      style: AppTextStyles.body,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
