2751357275babc942e3b6f75781f61db1cad21ae
Ranielison <ranielisonsoares2@gmail.com>
2021-04-27 00:05:22 -0300

    feat: criando o next button

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/widgets/next_button.dart/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,29 @@ class _ChallengePageState extends State<ChallengePage> {
       ),
       body: QuizWidget(
         title: 'O que o Flutter faz em sua totalidade?',
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: NextButtonWidget.white(
+                  label: 'Pular',
+                  onTap: () {},
+                ),
+              ),
+              SizedBox(width: 7),
+              Expanded(
+                child: NextButtonWidget.green(
+                  label: 'Confirmar',
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
